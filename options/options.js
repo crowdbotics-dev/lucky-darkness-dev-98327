619d32b99b3ec9acda5cf1362c3c441146d30aeb
Crowdbotics <engineering@crowdbotics.com>
2023-07-06 12:25:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lucky-darkness-dev-98327",
+  url: "https://lucky-darkness-dev-98327.botics.co",
+  api: "https://lucky-darkness-dev-98327.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
